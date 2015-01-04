f4df36122026d9fc7a61e2c61a19eefa806f1d6d
amree <mohd.amree@gmail.com>
2015-01-04 14:45:42 +0800

    Remove an item from the default menu and rename others

--- app/src/main/java/io/github/amree/campusonline/ecuti/MainActivity.java
+++ app/src/main/java/io/github/amree/campusonline/ecuti/MainActivity.java
@@ -64,9 +64,6 @@ public class MainActivity extends ActionBarActivity
             case 2:
                 mTitle = getString(R.string.title_section2);
                 break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
         }
     }
 
